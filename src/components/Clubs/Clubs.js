ba0938467e299c1eb2dfd61e5df93eada4a545ac
chardbres <richard.m.bresnahan@gmail.com>
2019-12-05 23:25:32 -0500

    Removed extraneous spaces to please npm deploy

--- src/components/Clubs/Clubs.js
+++ src/components/Clubs/Clubs.js
@@ -33,9 +33,8 @@ const Clubs = props => {
           variant: 'danger'
         })
       // .catch(console.error)
-    })
+      })
   }
-  
 
   const handleChange = event => {
     event.persist()
