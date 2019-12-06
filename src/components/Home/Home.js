ca2690fa60cd37c0f5aa8e0ba38b4ea07b6d4c65
chardbres <richard.m.bresnahan@gmail.com>
2019-12-06 09:31:55 -0500

    Added background image to home page and improved some styling.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,10 @@ const Home = props => {
   return (
     <div>
       <div className="welcome-screen">
-        <h1>Welcome Home!</h1>
-        <h2>Use Sign Up or Sign In to get started</h2>
+        <div>
+          <h1>Welcome Home!</h1>
+          <h2>Use Sign Up or Sign In to get started</h2>
+        </div>
       </div>
     </div>
   )
