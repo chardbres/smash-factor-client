0791d4a447ba064d95c778d1b1fa4a2ca7de2619
Rick Bresnahan <richard.m.bresnahan@gmail.com>
2020-01-03 15:17:27 -0500

    Changed URL in shots.js api

--- src/api/shots.js
+++ src/api/shots.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // Indexes all shots
 export const index = (id, user) => {
   return axios({
-    url: `${apiUrl}/shots`,
+    url: `${apiUrl}/clubs/${id}/shots`,
     method: 'GET',
     headers: {
       Authorization: `Bearer ${user.token}`
@@ -16,7 +16,7 @@ export const index = (id, user) => {
 // Creates a single shot
 export const createShot = (id, user, data) => {
   return axios({
-    url: `${apiUrl}/clubs/shots/${id}`,
+    url: `${apiUrl}/clubs/${id}/shots/${id}`,
     method: 'POST',
     headers: {
       Authorization: `Bearer ${user.token}`
